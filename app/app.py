3197a7f370f0afe6486897bd78cf88535e735257
cagansanli <s26942@pjwstk.edu.pl>
2026-01-09 15:07:13 +0100

    hello

--- app/app.py
+++ app/app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from predict import predict
 
-st.title("Iris Flower Prediction App Pro Max 🌸")
+st.title("Iris Flower Prediction App Pro Max ")
 
 sepal_length = st.number_input("Sepal Length", min_value=0.0, step=0.1)
 sepal_width = st.number_input("Sepal Width", min_value=0.0, step=0.1)
